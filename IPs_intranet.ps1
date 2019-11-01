58b51c4c418bff0d98a68938bece60ec511228c6
Wellington Rodrigues <35930902+BoJackBR@users.noreply.github.com>
2019-11-01 13:31:10 -0300

    Add file via upload

---
+++ IPs_intranet.ps1
@@ -0,0 +1,33 @@
+﻿Set-Location "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+Set-Location ZoneMap\Domains
+New-Item localhost
+Set-Location localhost
+New-ItemProperty . -Name * -Value 1 -Type DWORD
+
+Set-Location "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Ranges"
+New-Item Range1
+Set-Location Range1
+New-ItemProperty . -Name :Range -Value 192.168.3.1 -PropertyType String
+New-ItemProperty . -Name file -Value 1 -PropertyType DWORD
+cd ..
+
+Set-Location "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Ranges"
+New-Item Range2
+Set-Location Range2
+New-ItemProperty . -Name :Range -Value 192.168.3.3 -PropertyType String
+New-ItemProperty . -Name file -Value 1 -PropertyType DWORD
+cd ..
+
+Set-Location "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Ranges"
+New-Item Range3
+Set-Location Range3
+New-ItemProperty . -Name :Range -Value 192.168.3.29 -PropertyType String
+New-ItemProperty . -Name file -Value 1 -PropertyType DWORD
+cd ..
+
+Set-Location "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Ranges"
+New-Item Range4
+Set-Location Range4
+New-ItemProperty . -Name :Range -Value 192.168.3.40 -PropertyType String
+New-ItemProperty . -Name file -Value 1 -PropertyType DWORD
+cd ..
